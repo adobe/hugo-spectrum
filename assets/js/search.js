f6100cc58dfbaa5bb86c126e6a0f59c657cf720f
Alexander Falca <falca@adobe.com>
2020-02-24 09:35:32 -0500

    add license headers (#3)

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,22 @@
+/*
+ *
+ *    ADOBE CONFIDENTIAL
+ *    ___________________
+ *
+ *    Copyright 2020 Adobe Systems Incorporated
+ *    All Rights Reserved.
+ *
+ *    NOTICE:  All information contained herein is, and remains
+ *    the property of Adobe Systems Incorporated and its suppliers,
+ *    if any.  The intellectual and technical concepts contained
+ *    herein are proprietary to Adobe Systems Incorporated and its
+ *    suppliers and are protected by all applicable intellectual property
+ *    laws, including trade secret and copyright laws.
+ *    Dissemination of this information or reproduction of this material
+ *    is strictly forbidden unless prior written permission is obtained
+ *    from Adobe Systems Incorporated.
+ *
+ */
 (function($) {
     'use strict';
     var Search = {
@@ -7,14 +26,10 @@
                     var query = $(this).val();
                     var searchPage = "{{ "search/" | absURL }}?q=" + query;
                     document.location = searchPage;
-
                     return false;
                 });
-
             });
         },
     };
-
     Search.init();
-
 }(jQuery));
