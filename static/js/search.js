05a742c4eee329a8eaa2845e2d54d7464ed60a83
Adi Muraru <amuraru@adobe.com>
2020-09-16 11:47:18 +0300

    Improve search by ignoring where the searched term is looked up in content
    see https://fusejs.io/concepts/scoring-theory.html#scoring-theory

--- static/js/search.js
+++ static/js/search.js
@@ -16,10 +16,9 @@ var fuseOptions = {
   includeMatches: true,
   threshold: 0.01,
   tokenize:true,
-  location: 0,
-  distance: 100,
   maxPatternLength: 32,
   minMatchCharLength: 1,
+  ignoreLocation: true,
   keys: [
     {name:"title",weight:0.8},
     {name:"parent",weight:0.6},
