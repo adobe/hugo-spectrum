fea1229d6835d03a10c2fad40f50faf32cfdfd6d
Adrian Muraru <amuraru@adobe.com>
2020-09-21 15:31:10 +0300

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -19,6 +19,7 @@ var fuseOptions = {
   maxPatternLength: 32,
   minMatchCharLength: 1,
   ignoreLocation: true,
+  useExtendedSearch: true,
   keys: [
     {name:"title",weight:0.8},
     {name:"parent",weight:0.6},
