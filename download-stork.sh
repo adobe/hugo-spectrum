6bf15f9d52c10975b5d3d4a300ae03f378ac019c
Alexander Falca <falca@adobe.com>
2021-07-04 23:53:42 -0400

    add support for Stork search

---
+++ download-stork.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+#
+# Copyright 2021 Adobe
+# All Rights Reserved.
+#
+# NOTICE: Adobe permits you to use, modify, and distribute this file in
+# accordance with the terms of the Adobe license agreement accompanying
+# it. If you have received this file from a source other than Adobe,
+# then your use, modification, or distribution of it requires the prior
+# written permission of Adobe.
+#
+
+set -ex
+
+STORK_VERSION="v1.2.1"
+mkdir -p static/stork
+
+wget https://github.com/jameslittle230/stork/releases/download/$STORK_VERSION/stork.js -O static/js/stork.js
+wget https://github.com/jameslittle230/stork/releases/download/$STORK_VERSION/stork.wasm -O static/stork/stork.wasm
+
